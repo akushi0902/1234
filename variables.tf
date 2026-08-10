7a3b50b4431c6ad0d930ac87c476923e32a04751
akushi0902 <aadak@opsera.io>
2026-08-10 18:03:52 +0530

    CloudForge auto-fix (attempt 1): Removed module.subnet block from main.tf (it created a conflicting subnet with the same CIDR 10.0.1.0/24 as module.vpc's public subnet). Updated module.ec2 subnet_id to reference module.vpc.public_subnet_ids[0]. Updated outputs.tf to replace module.subnet.* references with module.vpc equivalents.

--- variables.tf
+++ variables.tf
@@ -41,22 +41,22 @@ variable "map_public_ip_on_launch" {
 
 variable "enable_dns_support" {
   type        = bool
-  description = "Enable DNS support in VPC"
+  description = "Enable DNS support"
 }
 
 variable "enable_dns_hostnames" {
   type        = bool
-  description = "Enable DNS hostnames in VPC"
+  description = "Enable DNS hostnames"
+}
+
+variable "single_nat_gateway" {
+  type        = bool
+  description = "Single NAT gateway"
 }
 
 variable "enable_nat_gateway" {
   type        = bool
   description = "Enable NAT gateway"
-}
-
-variable "single_nat_gateway" {
-  type        = bool
-  description = "Use single NAT gateway"
 }
 
 variable "subnet_name" {
@@ -69,9 +69,20 @@ variable "subnet_cidr_block" {
   description = "Subnet CIDR block"
 }
 
+variable "subnet_availability_zone" {
+  type        = string
+  description = "Availability zone for the subnet"
+}
+
 variable "subnet_map_public_ip_on_launch" {
   type        = bool
   description = "Map public IP on launch for subnet"
+}
+
+variable "subnet_ipv6_cidr_block" {
+  type        = string
+  description = "Subnet IPv6 CIDR block"
+  default     = null
 }
 
 variable "assign_ipv6_address_on_creation" {
@@ -79,15 +90,17 @@ variable "assign_ipv6_address_on_creation" {
   description = "Assign IPv6 address on creation"
 }
 
-variable "ipv6_cidr_block" {
-  type        = string
-  description = "IPv6 CIDR block for subnet"
-  default     = null
-}
-
-variable "create_route_table" {
-  type        = bool
-  description = "Create route table for subnet"
+variable "additional_routes" {
+  type = list(object({
+    cidr_block                = string
+    gateway_id                = optional(string)
+    nat_gateway_id            = optional(string)
+    transit_gateway_id        = optional(string)
+    vpc_peering_connection_id = optional(string)
+    network_interface_id      = optional(string)
+  }))
+  description = "Additional routes"
+  default     = []
 }
 
 variable "default_route_target_type" {
@@ -101,17 +114,9 @@ variable "default_route_target_id" {
   default     = null
 }
 
-variable "additional_routes" {
-  type = list(object({
-    cidr_block                = string
-    gateway_id                = optional(string)
-    nat_gateway_id            = optional(string)
-    transit_gateway_id        = optional(string)
-    vpc_peering_connection_id = optional(string)
-    network_interface_id      = optional(string)
-  }))
-  description = "Additional routes for subnet route table"
-  default     = []
+variable "create_route_table" {
+  type        = bool
+  description = "Create route table"
 }
 
 variable "security_group_name" {
@@ -135,7 +140,7 @@ variable "ingress_rules" {
     security_groups  = optional(list(string), [])
     self             = optional(bool, false)
   }))
-  description = "Security group ingress rules"
+  description = "Ingress rules"
   default     = []
 }
 
@@ -150,18 +155,18 @@ variable "egress_rules" {
     security_groups  = optional(list(string), [])
     self             = optional(bool, false)
   }))
-  description = "Security group egress rules"
+  description = "Egress rules"
   default     = []
 }
 
 variable "revoke_rules_on_delete" {
   type        = bool
-  description = "Revoke rules on security group delete"
+  description = "Revoke rules on delete"
 }
 
 variable "default_egress_allow_all" {
   type        = bool
-  description = "Default allow-all egress rule"
+  description = "Default egress allow all"
 }
 
 variable "iam_role_name" {
@@ -174,12 +179,13 @@ variable "assume_role_principals" {
     type        = string
     identifiers = list(string)
   }))
-  description = "IAM role trust policy principals"
+  description = "Assume role principals"
 }
 
 variable "managed_policy_arns" {
   type        = list(string)
-  description = "Managed policy ARNs to attach to IAM role"
+  description = "Managed policy ARNs"
+  default     = []
 }
 
 variable "iam_role_description" {
@@ -195,12 +201,12 @@ variable "iam_role_path" {
 
 variable "max_session_duration" {
   type        = number
-  description = "IAM role max session duration"
+  description = "Max session duration"
 }
 
 variable "force_detach_policies" {
   type        = bool
-  description = "Force detach policies on IAM role destroy"
+  description = "Force detach policies"
 }
 
 variable "inline_policies" {
@@ -208,19 +214,14 @@ variable "inline_policies" {
     name   = string
     policy = string
   }))
-  description = "IAM role inline policies"
+  description = "Inline policies"
   default     = {}
 }
 
 variable "permissions_boundary" {
   type        = string
-  description = "IAM role permissions boundary ARN"
+  description = "Permissions boundary ARN"
   default     = null
-}
-
-variable "iam_instance_profile_name" {
-  type        = string
-  description = "IAM instance profile name"
 }
 
 variable "ec2_name" {
@@ -246,17 +247,12 @@ variable "key_name" {
 
 variable "associate_public_ip" {
   type        = bool
-  description = "Associate public IP with EC2 instance"
+  description = "Associate public IP"
 }
 
-variable "ebs_delete_on_termination" {
-  type        = bool
-  description = "Delete EBS volume on termination"
-}
-
-variable "ebs_encrypted" {
-  type        = bool
-  description = "Encrypt EBS volume"
+variable "ebs_volume_size" {
+  type        = number
+  description = "EBS volume size"
 }
 
 variable "ebs_volume_type" {
@@ -264,19 +260,24 @@ variable "ebs_volume_type" {
   description = "EBS volume type"
 }
 
-variable "ebs_volume_size" {
-  type        = number
-  description = "EBS volume size in GB"
+variable "ebs_encrypted" {
+  type        = bool
+  description = "EBS encrypted"
+}
+
+variable "ebs_delete_on_termination" {
+  type        = bool
+  description = "EBS delete on termination"
 }
 
 variable "monitoring" {
   type        = bool
-  description = "Enable detailed CloudWatch monitoring"
+  description = "EC2 detailed monitoring"
 }
 
 variable "metadata_http_tokens" {
   type        = string
-  description = "IMDSv2 metadata HTTP tokens setting"
+  description = "Metadata HTTP tokens"
 }
 
 variable "metadata_http_put_response_hop_limit" {
@@ -284,14 +285,78 @@ variable "metadata_http_put_response_hop_limit" {
   description = "Metadata HTTP PUT response hop limit"
 }
 
-variable "user_data" {
-  type        = string
-  description = "EC2 user data script"
-  default     = null
+variable "log_groups" {
+  type = map(object({
+    name              = string
+    retention_in_days = optional(number, 14)
+    kms_key_id        = optional(string, null)
+    tags              = optional(map(string), {})
+  }))
+  description = "CloudWatch log groups"
+  default     = {}
 }
 
-variable "user_data_base64" {
-  type        = string
-  description = "EC2 user data base64 encoded"
-  default     = null
+variable "log_streams" {
+  type = map(object({
+    name           = string
+    log_group_name = string
+  }))
+  description = "CloudWatch log streams"
+  default     = {}
+}
+
+variable "metric_alarms" {
+  type = map(object({
+    alarm_name          = string
+    comparison_operator = string
+    evaluation_periods  = number
+    metric_name         = string
+    namespace           = string
+    period              = number
+    statistic           = string
+    threshold           = number
+    alarm_description   = optional(string, "")
+    alarm_actions       = optional(list(string), [])
+    ok_actions          = optional(list(string), [])
+    treat_missing_data  = optional(string, "missing")
+    datapoints_to_alarm = optional(number, null)
+    dimensions          = optional(map(string), null)
+    unit                = optional(string, null)
+    tags                = optional(map(string), {})
+  }))
+  description = "CloudWatch metric alarms"
+  default     = {}
+}
+
+variable "event_rules" {
+  type = map(object({
+    name                = string
+    description         = optional(string, "")
+    event_pattern       = optional(string, null)
+    schedule_expression = optional(string, null)
+    state               = optional(string, "ENABLED")
+    tags                = optional(map(string), {})
+  }))
+  description = "CloudWatch event rules"
+  default     = {}
+}
+
+variable "event_targets" {
+  type = map(object({
+    rule      = string
+    target_id = string
+    arn       = string
+    role_arn  = optional(string, null)
+  }))
+  description = "CloudWatch event targets"
+  default     = {}
+}
+
+variable "dashboards" {
+  type = map(object({
+    dashboard_name = string
+    dashboard_body = string
+  }))
+  description = "CloudWatch dashboards"
+  default     = {}
 }
