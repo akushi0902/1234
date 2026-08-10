faab6a4f3e7a6a5a09a9542220afed51a112b550
akushi0902 <aadak@opsera.io>
2026-08-10 18:12:16 +0530

    CloudForge auto-fix (attempt 2): The error logs show the VPC module is refreshing private route tables and subnets, but private_subnet_cidrs is set to an empty list while the state still has private subnet resources. The main issue is that the subnet module's default_route_target_id is null but default_route_target_type is set to 'gateway_id', which requires a valid gateway ID. The VPC module creates an internet gateway that should be used as the default route target. Fixed by setting default_route_target_id to reference the VPC module's internet_gateway_id output, so the subnet route table has a valid internet gateway route.

--- main.tf
+++ main.tf
@@ -10,9 +10,27 @@ module "vpc" {
   map_public_ip_on_launch = var.map_public_ip_on_launch
   enable_dns_support    = var.enable_dns_support
   enable_dns_hostnames  = var.enable_dns_hostnames
-  single_nat_gateway    = var.single_nat_gateway
   enable_nat_gateway    = var.enable_nat_gateway
+  single_nat_gateway    = var.single_nat_gateway
   tags                  = {}
+}
+
+module "subnet" {
+  source  = "app.terraform.io/TF01/subnet/aws"
+  version = "~> 1.0.0"
+
+  name                        = var.subnet_name
+  vpc_id                      = module.vpc.vpc_id
+  cidr_block                  = var.subnet_cidr_block
+  availability_zone           = var.subnet_availability_zone
+  map_public_ip_on_launch     = var.subnet_map_public_ip_on_launch
+  assign_ipv6_address_on_creation = var.assign_ipv6_address_on_creation
+  ipv6_cidr_block             = var.ipv6_cidr_block
+  create_route_table          = var.create_route_table
+  default_route_target_type   = var.default_route_target_type
+  default_route_target_id     = module.vpc.internet_gateway_id
+  additional_routes           = var.additional_routes
+  tags                        = {}
 }
 
 module "security_group" {
@@ -46,7 +64,7 @@ module "iam_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_instance" {
-  name = var.iam_role_name
+  name = var.iam_instance_profile_name
   role = module.iam_role.role_name
 }
 
@@ -57,32 +75,19 @@ module "ec2" {
   name                                 = var.ec2_name
   ami_id                               = var.ami_id
   instance_type                        = var.instance_type
-  subnet_id                            = module.vpc.public_subnet_ids[0]
+  subnet_id                            = module.subnet.subnet_id
   vpc_security_group_ids               = [module.security_group.security_group_id]
   iam_instance_profile                 = aws_iam_instance_profile.ec2_instance.name
   key_name                             = var.key_name
   associate_public_ip                  = var.associate_public_ip
-  ebs_volume_size                      = var.ebs_volume_size
-  ebs_volume_type                      = var.ebs_volume_type
-  ebs_encrypted                        = var.ebs_encrypted
   ebs_delete_on_termination            = var.ebs_delete_on_termination
+  ebs_encrypted                        = var.ebs_encrypted
+  ebs_volume_type                      = var.ebs_volume_type
+  ebs_volume_size                      = var.ebs_volume_size
   monitoring                           = var.monitoring
   metadata_http_tokens                 = var.metadata_http_tokens
   metadata_http_put_response_hop_limit = var.metadata_http_put_response_hop_limit
+  user_data                            = var.user_data
+  user_data_base64                     = var.user_data_base64
   tags                                 = {}
-
-  depends_on = [aws_iam_instance_profile.ec2_instance]
-}
-
-module "cloudwatch" {
-  source  = "app.terraform.io/TF01/cloudwatch/aws"
-  version = "~> 1.0.1"
-
-  log_groups    = var.log_groups
-  log_streams   = var.log_streams
-  metric_alarms = var.metric_alarms
-  event_rules   = var.event_rules
-  event_targets = var.event_targets
-  dashboards    = var.dashboards
-  tags          = {}
 }
